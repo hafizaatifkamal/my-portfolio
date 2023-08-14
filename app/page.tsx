a56801bf51c94208d3969103fd61a2fdb592a6bc
hafizaatifkamal <hafizaatifkamal@gmail.com>
2023-08-15 00:14:45 +0530

    view changed pages to components structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import HomeIntro from "./components/Intro";
+import About from "./components/sections/About";
+import Experience from "./components/sections/Experience";
+import Contact from "./components/sections/Contact";
+import Projects from "./components/sections/Projects";
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="container mx-auto flex-grow">
-        <HomeIntro />
+      <main className="container mx-auto flex-grow overflow-hidden lg:overflow-auto">
+        <HomeIntro id="intro" />
+        <About id="about" />
+        <Projects id="projects" />
+        <Experience id="experience" />
+        <Contact id="contact" />
       </main>
     </div>
   );
