a684819f5f6a3af7e0e2ac6ee97770d949020f85
hafizaatifkamal <hafizaatifkamal@gmail.com>
2023-08-15 00:16:06 +0530

    gradient colors added

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="shadow-lg bg-gradient-to-r from-gray-400 to-gray-900 text-white py-6">
       <div className="container mx-auto text-center flex items-center justify-center h-full">
         <p>
           &copy; {new Date().getFullYear()} Atif Kamal. All rights reserved.
